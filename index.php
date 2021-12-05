e2e0b50ae2737693cbf2bc5d998a207fedfb9bfe
Arasyi D R <73739482+Arss-20@users.noreply.github.com>
2021-12-05 19:57:59 +0700

    Add files via upload
    menambahkan file php dan css

---
+++ index.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Home</title>
+
+	<!-- Styles -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:400,400i,600,700,700i&amp;subset=latin-ext" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://unpkg.com/swiper/swiper-bundle.css" rel="stylesheet">
+    <link href="https://unpkg.com/swiper/swiper-bundle.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.css" integrity="sha512-WEQNv9d3+sqyHjrqUZobDhFARZDko2wpWdfcpv44lsypsSuMO0kHGd3MQ8rrsBn/Qa39VojphdU6CMkpJUmDVw==" crossorigin="anonymous" />
+    <link href="css/css.css" rel="stylesheet">
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+
+    <!-- Favicon  -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.1/font/bootstrap-icons.css">
+    <link rel="icon" href="img/thumbnail.png">
+</head>
+
+<body>
+	<body data-spy="scroll" data-target=".fixed-top">
+    <!-- Kontak paling atas -->
+    <div class="container-fluid">
+      <div class="row row-cols-auto py-2 px-3">
+        <div class="col-auto mx-4">
+            <i class="bi bi-telephone-fill"></i>
+            +62890800628
+        </div>
+        <div class="col-auto mx-4">
+            <i class="bi bi-envelope-fill"></i>
+            Ekantinunj@unj.ac.id
+        </div>
+        <div class="col-6"></div>
+      </div>
+    </div>
+
+    <!-- Navigation -->
+    <nav class="navbar fixed-top navbar-expand-lg navbar-light  navbar-custom sticky-top bgcolor ">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">E-KANTIN UNJ</a>
+        
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-awesome fas fa-bars"></span>
+        </button>
+        
+        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a href="#" class="nav-link page-scroll">
+                <i class="bi bi-house-door-fill"></i>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link page-scroll" href="#header">HOME<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link page-scroll" href="#about" >KANTIN</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link page-scroll" href="#modul">TENTANG KAMI</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link page-scroll" href="journal.html">KONTAK KAMI</a>
+            </li> 
+          </ul>
+        
+          <span class="nav-item social-icons">
+            <span class="fa-stack">
+                <a href="https://facebook.com">
+                    <i class="fas fa-shopping-cart fa-stack-2x"></i>
+                </a>
+            </span>
+            <span class="fa-stack">
+                <a href="https://twitter.com">
+                    <i class="fas fa-bell fa-stack-2x"></i>
+                </a>
+            </span>
+            <span class="fa-stack">
+                <a href="https://twitter.com">
+                    <i class="fas fa-user fa-stack-2x"></i>
+                </a>
+            </span>
+          </span>
+        </div>
+      </div>
+    </nav>
+
+
+    <!--- Header -->
+    <div id="header" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="img/header1.jpg" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="img/header2.jpg" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="img/header3.jpg" class="d-block w-100" alt="...">
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#header" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#header" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+    </div>
+
+    <!-- Footer -->
+    <footer class="text-lg-start">
+      <!-- Grid container -->
+      <div class="container-fluid p-4">
+        <!--Grid baris dengan 3 kolom-->
+        <div class="row row-cols-3">
+          <div class="col-lg-5 ms-2 mb-md-0">
+            <p class="h1 fw-bold">E-KANTIN UNJ</p>
+          </div>
+
+          <!--Kolom nama kantin-->
+          <div class="col-lg-4 mb-4 mb-md-0">
+            <p class="h2 fw-bold">Kantin</p>
+            <div class="row">
+              <div class="col-lg-3">Blok M</div>
+              <div class="col-lg-3">Hello Kitty</div>
+            </div>
+            <div class="row ">
+              <div class="col-lg-3">Gedung G</div>
+              <div class="col-lg-3">Kantin Hijau</div>
+            </div>
+            <div class="row ">
+              <div class="col-lg-3">Cafe Kebun</div>
+              <div class="col-lg-3">Terace Cafe</div>
+            </div>
+          </div>
+
+          <!--Kolom lainnya-->
+          <div class="col-lg-auto mb-4 mb-md-0">
+            <p class="h2 fw-bold">Lainnya</p>
+
+            <div class="row row-cols-auto">
+              <div class="col">Tentang Kami</div>
+            </div>
+            <div class="row row-cols-auto">
+              <div class="col">Kontak Kami</div>
+            </div>
+            <div class="row row-cols-auto">
+              <div class="col">Profil</div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+        <!-- Copyright -->
+      <div class="text-start text-white px-4 py-2">
+        © 2021 E-Kantin UNJ. All Rights Reserved
+      </div>
+    </footer>
+    <!-- Footer -->
+
+
+    <!-- Scripts -->
+    <script>
+        $(window).scroll(function(){
+        $('nav').toggleClass('scrolled',$(this).scrollTop()>50);
+        });
+    </script>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js" integrity="sha512-0QbL0ph8Tc8g5bLhfVzSqxe9GERORsKhIn1IrpxDAgUsbBGz/V7iSav2zzW325XGd1OMLdL4UiqRJj702IeqnQ==" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/swiper/swiper-bundle.js"></script> 
+    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script> 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.js" integrity="sha512-C1zvdb9R55RAkl6xCLTPt+Wmcz6s+ccOvcr6G57lbm8M2fbgn2SUjUJbQ13fEyjuLViwe97uJvwa1EUf4F1Akw==" crossorigin="anonymous"></script>
+
+    <!-- Scripts -->
+
+
+</body>
+</html>
